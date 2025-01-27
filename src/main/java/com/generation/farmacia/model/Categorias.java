1ce9a860322eaf141850f320327f312c2e6344f8
PaolaPatricia16 <patricia-9856@hotmail.com>
2025-01-27 15:11:21 -0300

    CRUD-Produto

--- src/main/java/com/generation/farmacia/model/Categorias.java
+++ src/main/java/com/generation/farmacia/model/Categorias.java
@@ -1,9 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.generation.farmacia.model.Produtos;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -24,8 +32,22 @@ public class Categorias {
 	@Size( min = 5, max = 300)
 	private String descricao;
 
+	@OneToMany (fetch = FetchType.LAZY, mappedBy = "categorias", cascade = CascadeType.REMOVE)
+
+	@JsonIgnoreProperties("categorias")
+	private List<Produtos> produtos;
 	
 	
+	
+	
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produtos> produtos) {
+		this.produtos = produtos;
+	}
+
 	public Long getId() {
 		return id;
 	}
