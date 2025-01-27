b1f85e49dd378daf2592bde98820e56da24ca0b3
PaolaPatricia16 <patricia-9856@hotmail.com>
2025-01-27 14:35:57 -0300

    CRUD - Categoria

---
+++ src/main/java/com/generation/farmacia/repository/CategoriasRepository.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.farmacia.model.Categorias;
+
+public interface CategoriasRepository extends JpaRepository <Categorias, Long>{
+	public List<Categorias> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
+
+}
